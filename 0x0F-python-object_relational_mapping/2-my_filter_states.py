ee0f877c864035052f6e7e54416099632ec33789
Golimaat <111962750+Golimaat@users.noreply.github.com>
2023-03-24 23:12:30 +0300

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,36 @@
+
+devjokar
+/
+alx-higher_level_programming
+Public
+Code
+Issues
+Pull requests
+Actions
+More
+alx-higher_level_programming/0x0F-python-object_relational_mapping/2-my_filter_states.py /
+
+Josi Karachi Chmod all task files
+ 0 contributors
+Executable File  21 lines (18 sloc)  525 Bytes
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
+"""
+Lists all values in the states tables of a database where name
+matches the argument
+"""
 import sys
+import MySQLdb
 
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
